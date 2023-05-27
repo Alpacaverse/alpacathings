230fd632984d853c676575c1fa48df9c4bfd2674
jolinroulin <jolinroulin5805@gmail.com>
2023-05-28 00:32:01 +0800

    Update CheckoutDetails.jsx

--- components/Checkout/CheckoutDetails.jsx
+++ components/Checkout/CheckoutDetails.jsx
@@ -4,24 +4,28 @@ import {
   Radio,
   RadioGroup,
   Stack,
+  Grid,
+  GridItem,
   HStack,
   Button,
+  Heading,
   Image,
   useColorModeValue,
   Text,
   Icon,
   Spacer,
   Center,
+  Link,
 } from "@chakra-ui/react";
 import { EditIcon } from "@chakra-ui/icons";
 
 const CheckoutDetails = (props) => {
   return (
     <>
-      <Text fontSize={18} fontWeight={"bold"}>
+      <Text fontSize={"xl"} fontWeight={"bold"}>
         Checkout
       </Text>
-      <Divider borderWidth="1px" borderColor="green.800" borderStyle="dashed" />
+      <Divider borderWidth="1px" borderColor="gray.400" />
       <Stack direction={"row"} alignItems={"center"}>
         <Text fontSize={18} fontWeight={"bold"}>
           Address
@@ -36,15 +40,19 @@ const CheckoutDetails = (props) => {
           height="" // Sets the height to 100% of the viewport height
           bg="green.100" // Example background color
         >
-          <Text padding={2} fontSize={16}>
+          <Text
+            color={useColorModeValue("black", "black")}
+            padding={2}
+            fontSize={"medium"}
+          >
             Tower 2a Ave 5 Bangsar South 8 Jalan Kerinchi, Kuala Lumpur, Kuala
             Lumpur, 59200, Malaysia
           </Text>
         </Box>
       </Stack>
-      <Divider borderWidth="1px" borderColor="green.800" borderStyle="dashed" />
+      <Divider borderWidth="1px" borderColor="gray.400" />
 
-      <Text fontSize={18} fontWeight={"bold"}>
+      <Text fontSize={"18"} fontWeight={"bold"}>
         Delivery Method
       </Text>
       <RadioGroup defaultValue="2">
@@ -58,7 +66,8 @@ const CheckoutDetails = (props) => {
         </Stack>
       </RadioGroup>
 
-      <Divider borderWidth="1px" borderColor="green.800" borderStyle="dashed" />
+      <Divider borderWidth="1px" borderColor="gray.400" />
+
       <Text fontSize={18} fontWeight={"bold"}>
         Payment Method
       </Text>
@@ -132,7 +141,60 @@ const CheckoutDetails = (props) => {
           </HStack>
         </Stack>
       </RadioGroup>
-      <Divider borderWidth="1px" borderColor="green.800" borderStyle="dashed" />
+      <Divider borderWidth="1px" borderColor="gray.400" />
+
+      <Text fontSize={"xl"} fontWeight={"bold"}>
+        Shopping Bill
+      </Text>
+      <Heading as={"h4"} fontSize={"lg"} fontWeight={"semibold"}>
+        <Grid templateColumns={"repeat(3,1fr)"}>
+          <GridItem color={useColorModeValue("gray.500", "white")} colSpan={2}>
+            1 x Rice Cooker
+          </GridItem>
+          <GridItem
+            colSpan={1}
+            textAlign={"right"}
+            color={useColorModeValue("gray.500", "white")}
+          >
+            <Box as={"span"} fontSize={"xs"}>
+              RM
+            </Box>
+            25.00
+          </GridItem>
+        </Grid>
+      </Heading>
+      <Divider
+        borderWidth="2px"
+        borderColor={useColorModeValue("gray.800", "white")}
+        borderStyle="dashed"
+      />
+      <Heading as={"h"} fontSize={"xl"} fontWeight={"semibold"}>
+        <Grid templateColumns={"repeat(3,1fr)"}>
+          <GridItem color={useColorModeValue("black.500", "white")} colSpan={2}>
+            Total Bill:
+          </GridItem>
+          <GridItem
+            colSpan={1}
+            textAlign={"right"}
+            color={useColorModeValue("black.500", "white")}
+          >
+            <Box as={"span"} fontSize={"xs"}>
+              RM
+            </Box>
+            25.00
+          </GridItem>
+        </Grid>
+      </Heading>
+      <Divider
+        borderWidth="2px"
+        borderColor={useColorModeValue("gray.800", "white")}
+        borderStyle="dashed"
+      />
+      <Box></Box>
+
+      <Button h={10} variant={"outline"} colorScheme={"green"} size="xl">
+        <Link href="./transactionHistory"> Confirm Payment</Link>
+      </Button>
     </>
   );
 };
